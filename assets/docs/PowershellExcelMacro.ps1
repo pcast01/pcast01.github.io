becdcf93b328bdfa214573ad603d058b956de7e9
Paul Brandon Castillo <azrael7@gmail.com>
2017-01-25 22:07:40 -0600

    Added files to start excel macro post

---
+++ assets/docs/PowershellExcelMacro.ps1
@@ -0,0 +1,14 @@
+﻿Function RunExcelMacro() {
+    # Open Excel file
+    $excel = new-object -comobject excel.application
+    $filePath = "C:\Users\azrae\Downloads\minimal-mistakes-master\pcast01blog\assets\docs\PowershellSheet.xlsm"
+    $workbook = $excel.Workbooks.Open($FilePath)
+    $excel.Visible = $true
+    $worksheet = $workbook.worksheets.item(1)
+    Write-Host "Running macro in excel to scrub data."
+    $excel.Run("PowershellMacro")
+    $workbook.save()
+    $workbook.close()
+    $excel.quit()
+    Write-Host "Closed Excel"
+}
